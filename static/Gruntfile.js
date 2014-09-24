aa98be15c92c20cab52eb132a1d6586cd00012ae
Joao Sa <me@joaomsa.com>
2014-09-24 00:27:49 -0300

    Add gruntfile for compiling less

---
+++ static/Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        paths: {
+            "less": "style/less",
+            "css": "style/css"
+        },
+
+        less: {
+            development: {
+                files: {
+                    "<%= paths.css %>/style.css": "<%= paths.less %>/style.less"
+                }
+            }
+        },
+
+        watch: {
+            style: {
+                files: [
+                    "<%= paths.less %>/*.less"
+                ],
+                tasks: ["less"]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.registerTask("default", ["watch"]);
+}
